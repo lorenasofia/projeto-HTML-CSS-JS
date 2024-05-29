d6b35db530622896e11c22b60a25afa7e171a402
Aline <alinebarantes@gmail.com>
2024-05-29 00:03:43 -0300

    modify dark mode js

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', function () {
     themeButton.addEventListener('click', function () {
         document.body.classList.toggle('dark-mode');
         
-        // Muda o texto do botão conforme o modo
         if (document.body.classList.contains('dark-mode')) {
             themeButton.textContent = 'Modo Claro (colocar icon)';
         } else {

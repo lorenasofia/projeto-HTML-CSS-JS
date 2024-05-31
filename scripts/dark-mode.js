e8002446a7249a1e06fa01cbd69177fba3c5c347
Aline <alinebarantes@gmail.com>
2024-05-31 18:44:21 -0300

    fix modo escuro

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const themeButton = document.querySelector('.change-theme-button');
+    const buttonText = themeButton.querySelector('span');
+    const darkThemeIcon = '/assets/icons/dark-theme.svg';
+    const lightThemeIcon = '/assets/icons/dark-theme.svg';
 
     themeButton.addEventListener('click', function () {
         document.body.classList.toggle('dark-mode');
         
         if (document.body.classList.contains('dark-mode')) {
-            themeButton.textContent = 'Modo Claro (colocar icon)';
+            themeButton.querySelector('img').src = lightThemeIcon;
+            buttonText.textContent = 'Modo Claro';
         } else {
-            themeButton.textContent = 'Modo Escuro (colocar icon)';
+            themeButton.querySelector('img').src = darkThemeIcon;
+            buttonText.textContent = 'Modo Escuro';
         }
     });
 });
